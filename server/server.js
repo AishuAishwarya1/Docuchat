f6f64e3198c517aadd1d35fd625d7e4202327179
Aishwarya <aishwaryamahendran1@gmail.com>
2026-08-04 16:31:36 +0530

    Pre-deployment cleanup

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
